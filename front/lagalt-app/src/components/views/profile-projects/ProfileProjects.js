e685b5ab45a0206f42b61248059dc42b370400f0
jessemaa <mewwbae@gmail.com>
2021-03-17 12:25:03 +0200

    feat: add create project button

--- front/lagalt-app/src/components/views/profile-projects/ProfileProjects.js
+++ front/lagalt-app/src/components/views/profile-projects/ProfileProjects.js
@@ -1,10 +1,12 @@
 import ProfileNavbar from '../profile-page/profile-navbar/ProfileNavbar'
 import ProfileProjectsGrid from './profile-projects-grid/ProfileProjectsGrid'
+import { Button } from 'react-bootstrap'
 
 function ProfileProjects() {
     return (
         <div>
             <ProfileNavbar />
+            <Button variant="success">Create Project</Button>
             <ProfileProjectsGrid />
         </div>
     )
