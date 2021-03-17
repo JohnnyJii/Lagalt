91f17d7880a1d841b517193b880ef4357a4b5c0f
jessemaa <mewwbae@gmail.com>
2021-03-17 11:20:40 +0200

    feat: add routing to profile page

--- front/lagalt-app/src/App.js
+++ front/lagalt-app/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import ProfilePage from './components/views/profile-page/ProfilePage';
 import ProfileProjects from './components/views/profile-projects/ProfileProjects'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <ProfilePage />
-      <ProfileProjects />
-    </div>
+    <Router>
+      <div className="App">
+        <main>
+          <Switch>
+            <Route path="/profile" component={ProfilePage} />
+            <Route path="/projects" component={ProfileProjects} />
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
--- front/lagalt-app/src/components/views/profile-page/profile-navbar/ProfileNavbar.js
+++ front/lagalt-app/src/components/views/profile-page/profile-navbar/ProfileNavbar.js
@@ -1,13 +1,18 @@
 import { Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function ProfileNavbar() {
     return(
         <Nav variant="tabs" defaultActiveKey="/profile">
             <Nav.Item>
-                <Nav.Link>Profile</Nav.Link>
+                <Nav.Link>
+                    <Link to="/profile">Profile</Link>
+                </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link>My Projects</Nav.Link>
+                <Nav.Link>
+                    <Link to="/projects">My Projects</Link>
+                </Nav.Link>
             </Nav.Item>
         </Nav>
     )
