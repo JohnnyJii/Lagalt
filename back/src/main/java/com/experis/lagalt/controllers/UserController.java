f4ace94410f20ab0ac5afdb92770231e5838b207
veliValentine <nikkevanukas1995@gmail.com>
2021-03-19 12:46:13 +0200

    feat: User controller getAll

--- back/src/main/java/com/experis/lagalt/controllers/UserController.java
+++ back/src/main/java/com/experis/lagalt/controllers/UserController.java
@@ -1,5 +1,25 @@
 package com.experis.lagalt.controllers;
 
+import com.experis.lagalt.models.User;
+import com.experis.lagalt.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = ControllerHelpers.API_V1 + "/users")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers() {
+        HttpStatus status = HttpStatus.OK;
+        List<User> users = userRepository.findAll();
+        return new ResponseEntity<>(users, status);
+    }
 }
