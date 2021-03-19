e859cfce77f55d1ae080dba3db7cc8381f854052
veliValentine <nikkevanukas1995@gmail.com>
2021-03-19 12:35:34 +0200

    feat: User model! No mapping to projects

---
+++ back/src/main/java/com/experis/lagalt/models/User.java
@@ -0,0 +1,108 @@
+package com.experis.lagalt.models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "eMail")
+    private String eMail;
+
+    private String firstName;
+
+    private String lastName;
+
+    /*
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "users")
+    @Column("project_key")
+    private Set<ProjectModel> projects;
+     */
+
+    private String imageSource;
+
+    private String skills;
+
+    private String description;
+
+    /*
+    public User() {
+        projects = new HashSet<>();
+    }
+     */
+
+    // Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getImageSource() {
+        return imageSource;
+    }
+
+    public void setImageSource(String imageSource) {
+        this.imageSource = imageSource;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- back/src/main/java/com/experis/lagalt/models/UserModel.java
+++
@@ -1,4 +0,0 @@
-package com.experis.lagalt.models;
-
-public class UserModel {
-}
