10d4821b97448ffcafd9d4c0d3be6d9b54289679
LLehtola <56760014+LLehtola@users.noreply.github.com>
2021-03-22 09:13:28 +0200

    Build: initial build of class project

---
+++ back/src/main/java/com/experis/lagalt/models/Project.java
@@ -0,0 +1,84 @@
+package com.experis.lagalt.models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "industry")
+    private String industry;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "gitlink")
+    private String gitlink;
+
+    @ElementCollection
+    private Set<String> skills;
+
+    public Project() {
+        //projects = new HashSet<>();
+        skills = new HashSet<>();
+    }
+
+    // Getters and setters
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getGitlink() {
+        return gitlink;
+    }
+
+    public void setGitlink(String gitlink) {
+        this.gitlink = gitlink;
+    }
+
+    public Set<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<String> skills) {
+        this.skills = skills;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
