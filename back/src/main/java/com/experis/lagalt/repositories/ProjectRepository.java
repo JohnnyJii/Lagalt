099a83d20d53976c73d45e1501333a941d9a5867
veliValentine <nikkevanukas1995@gmail.com>
2021-03-23 10:25:13 +0200

    fix: class name was camelCase

--- back/src/main/java/com/experis/lagalt/repositories/ProjectRepository.java
+++ back/src/main/java/com/experis/lagalt/repositories/ProjectRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface projectRepository extends JpaRepository<User, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
