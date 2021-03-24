0b089a5b0ba92067d9c61ae0bdaf210a9973d884
JohnnyJii <jukka.juntunen@metropolia.fi>
2021-03-24 11:29:44 +0200

    feat: searchbar working, not connected to api

--- front/lagalt-app/src/components/shared/navbar/Nav.js
+++ front/lagalt-app/src/components/shared/navbar/Nav.js
@@ -3,6 +3,29 @@ import './StyleNav.css'
 import { Link } from 'react-router-dom'
 
 function About() {
+
+   // API here!!! modify to read projects and users!!!!
+
+  const projects = [
+    "jukka",
+    "jesse",
+    "lauri",
+    "nicolas"
+  ]
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+ };
+
+ const results = !searchTerm
+    ? projects
+    : projects.filter(project =>
+        project.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      );
+
+
+
   // NEED A STATE FOR USERNAME CHANGES!!!!
   let userName = localStorage.getItem('userName')
   return (
@@ -11,6 +34,14 @@ function About() {
         <Link to="/">
           <h1>Lagalt</h1>
         </Link>
+      </div>
+      <div>
+        <input 
+        type="text"
+        placeholder="search"
+        value={searchTerm}
+        onChange={handleChange}
+        />
       </div>
       <div className="navbar-right">
         { userName ? 
