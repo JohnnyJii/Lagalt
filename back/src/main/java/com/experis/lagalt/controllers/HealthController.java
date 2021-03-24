8f8b98d8329d5bddf226dd0e4b835597c955c978
veliValentine <nikkevanukas1995@gmail.com>
2021-03-24 11:39:56 +0200

    fix: allowed cross-origins to everything

--- back/src/main/java/com/experis/lagalt/controllers/HealthController.java
+++ back/src/main/java/com/experis/lagalt/controllers/HealthController.java
@@ -2,10 +2,12 @@ package com.experis.lagalt.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HealthController {
 
     @GetMapping(value = ControllerHelpers.API_V1 + "/health")
