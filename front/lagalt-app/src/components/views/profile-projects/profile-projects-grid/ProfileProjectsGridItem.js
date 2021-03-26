d1a58b287cef841e4eae06de0f3b529d1e5850f4
jessemaa <mewwbae@gmail.com>
2021-03-26 12:42:30 +0200

    feat: landing page carousel

--- front/lagalt-app/src/components/views/profile-projects/profile-projects-grid/ProfileProjectsGridItem.js
+++ front/lagalt-app/src/components/views/profile-projects/profile-projects-grid/ProfileProjectsGridItem.js
@@ -1,25 +1,28 @@
 import './ProfileProjectsGridItem.css'
 import React from 'react'
 import ProfileProjectsModal from '../profile-projects-modal/ProfileProjectsModal'
+import { Card } from 'react-bootstrap'
+
 
 
 function ProfileProjectsGridItem(props) {
     const [modalShow, setModalShow] = React.useState(false);
 
     return(
-        <section className="container-flex" id="projects">
-            <div className="row">
-                <div className="col-md-4 col-lg-3 col-12 project-card-wrapper" onClick={() => setModalShow(true)}>
-                    <div className="col-12 project-card">
-                        <div className="project-card-content">
-                            <h4><b>Project Name</b></h4>
-                            <p>Short description</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <ProfileProjectsModal id={props.id} show={modalShow} onHide={() => setModalShow(false)} />
-        </section>
+        <div style={{margin: "20px", cursor: "pointer"}}>
+            <Card onClick={() => setModalShow(true)}>
+                <Card.Header>{props.industry ? props.industry : 'Industry'}</Card.Header>
+                <Card.Body>
+                    <Card.Title>{props.title ? props.title : 'Title'}</Card.Title>
+                    <Card.Text>{props.desc ? props.desc : 'Description'}</Card.Text>
+                </Card.Body>
+            </Card>
+            <ProfileProjectsModal
+                id={props.id}
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
+        </div>
     )
 }
 
