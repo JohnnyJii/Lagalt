3a232266105d3a171e90b3530c7e6b29a452133b
veliValentine <nikkevanukas1995@gmail.com>
2021-03-30 15:20:46 +0300

    feat: Unauthorized message

---
+++ back/src/main/java/com/experis/lagalt/security/util/AuthEntryPointJwt.java
@@ -0,0 +1,20 @@
+package com.experis.lagalt.security.util;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException{
+        // TODO REMOVE SOUT
+        System.out.println("COMMENCE!");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+    }
+}
