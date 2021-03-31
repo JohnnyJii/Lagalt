9c9c19266064b3af5628783030eee182af5f8747
veliValentine <nikkevanukas1995@gmail.com>
2021-03-31 16:23:21 +0300

    feat: Role repository

---
+++ back/src/main/java/com/experis/lagalt/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.experis.lagalt.repositories;
+
+import com.experis.lagalt.models.role.Role;
+import com.experis.lagalt.models.role.RoleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByRole(RoleType role);
+}
