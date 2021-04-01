602523b5b7aa059cc4728e9012b669e33b4eaf59
veliValentine <nikkevanukas1995@gmail.com>
2021-04-01 16:46:33 +0300

    refactor: helper methods for clean code

--- back/src/main/java/com/experis/lagalt/security/jwt/AuthTokenFilter.java
+++ back/src/main/java/com/experis/lagalt/security/jwt/AuthTokenFilter.java
@@ -2,9 +2,9 @@ package com.experis.lagalt.security.jwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -29,18 +29,13 @@ public class AuthTokenFilter extends OncePerRequestFilter {
             if (jwt != null && jwtUtil.validateJwtToken(jwt)) {
                 UserDetails user = jwtUtil.getUser(jwt);
                 UsernamePasswordAuthenticationToken authToken = authToken(user);
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request));
+                authToken.setDetails(setBuildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
         filterChain.doFilter(request, response);
-    }
-
-    private UsernamePasswordAuthenticationToken authToken(UserDetails user) {
-        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
     }
 
     private String parseJwt(HttpServletRequest httpServletRequest) {
@@ -50,4 +45,13 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         }
         return null;
     }
+
+    private UsernamePasswordAuthenticationToken authToken(UserDetails user) {
+        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+    }
+
+    private WebAuthenticationDetails setBuildDetails(HttpServletRequest request) {
+        return new WebAuthenticationDetailsSource().buildDetails(request);
+    }
+
 }
