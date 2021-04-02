fe615921d690c7e6fd854b7b95af972817b80093
veliValentine <nikkevanukas1995@gmail.com>
2021-04-02 14:03:20 +0300

    style: whitespace

--- back/src/main/java/com/experis/lagalt/repositories/UserRepository.java
+++ back/src/main/java/com/experis/lagalt/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.experis.lagalt.repositories;
 import com.experis.lagalt.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
