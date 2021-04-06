6210b892aabf428583770fbc95375cb3beccc928
veliValentine <nikkevanukas1995@gmail.com>
2021-04-06 16:49:09 +0300

    feat: users urls

--- front/lagalt-app/src/utils/serverUrl.js
+++ front/lagalt-app/src/utils/serverUrl.js
@@ -1,3 +1,9 @@
 export const PROJECTS_URL = 'https://lagalt-server.herokuapp.com/api/v1/projects';
 
 export const PROJECT_URL = (projectId) => `${PROJECTS_URL}/${projectId}`;
+
+export const USERS_PROJECTS_URL = (userId) => `${USER_URL(userId)}/projects`
+
+export const USER_URL = (userId) => `${USERS_URL}/${userId}`
+
+export const USERS_URL = 'https://lagalt-server.herokuapp.com/api/v1/users';
