0e56e5168a6fc8718f0804f59d1a4bff8e97640e
veliValentine <nikkevanukas1995@gmail.com>
2021-04-07 19:36:01 +0300

    refactor: own container for new projects

--- front/lagalt-app/src/components/views/profile-view/my-projects/ProfileProjectsX.js
+++ front/lagalt-app/src/components/views/profile-view/my-projects/ProfileProjectsX.js
@@ -5,29 +5,37 @@ import React from 'react';
 import useUserProjects from '../../../../hooks/useUserProjects';
 
 function ProfileProjectsX(props) {
-  const [modalShow, setModalShow] = React.useState(false);
   const dbuserid = props.dbuser.id;
   const [projects, addProject] = useUserProjects(dbuserid);
-
   return (
     <div>
-      <div style={{ textAlign: 'center' }}>
-        <CreateProjectX
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-          dbuserid={dbuserid}
-          addProject={addProject} />
-        <Button
-          variant="primary"
-          onClick={() => setModalShow(true)}>
-                    Create Project
-        </Button>
-      </div>
+      <NewProjectContainer
+        dbuserid={dbuserid}
+        addProject={addProject}
+      />
       <ProfileProjectsGridX
         projects={projects}
       />
     </div>
   );
 }
+
+const NewProjectContainer = function ({ dbuserid, addProject }) {
+  const [modalShow, setModalShow] = React.useState(false);
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <CreateProjectX
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        dbuserid={dbuserid}
+        addProject={addProject} />
+      <Button
+        variant="primary"
+        onClick={() => setModalShow(true)}>
+        Create Project
+      </Button>
+    </div>
+  );
+};
 
 export default ProfileProjectsX;
