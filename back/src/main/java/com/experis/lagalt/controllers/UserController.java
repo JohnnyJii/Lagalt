e20438bb49f5188c42bd04179b2e77a231bc9429
lauri.lehtola <mr.lauri.lehtola@gmail.com>
2021-04-08 08:38:14 +0300

    feat: get new content for user (ordered list of projects by user skills)

--- back/src/main/java/com/experis/lagalt/controllers/UserController.java
+++ back/src/main/java/com/experis/lagalt/controllers/UserController.java
@@ -125,4 +125,16 @@ public class UserController {
         List<Project> projects = userService.getUserProjectsPartOf(userId);
         return new ResponseEntity<>(projects, HttpStatus.OK);
     }
+
+    @GetMapping(value = "/{userId}/projects/newcontent")
+    public ResponseEntity<List<Project>> getProjectsNewContentForUser(@PathVariable long userId){
+        List<Project> projects = userService.getProjectsNewContentForUser(userId);
+        HttpStatus status;
+        if (userService.userExists(userId)) {
+            status = HttpStatus.OK;
+        } else {
+            status = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(projects, status);
+    }
 }
