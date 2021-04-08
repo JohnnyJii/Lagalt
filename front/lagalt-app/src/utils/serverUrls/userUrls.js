f1288f430b81629ff92b8f242a69fe5d004ce3e8
veliValentine <nikkevanukas1995@gmail.com>
2021-04-08 19:27:32 +0300

    feat: get recommended projects and show them on carusel

--- front/lagalt-app/src/utils/serverUrls/userUrls.js
+++ front/lagalt-app/src/utils/serverUrls/userUrls.js
@@ -7,3 +7,5 @@ export const USERS_PROJECTS_URL = (userId) => `${USER_URL(userId)}/projects`;
 export const USER_BY_GOOGLE_ID = (googleId) => `${USERS_URL}/googleid/${googleId}`;
 
 export const USER_PROJECTS_PARTICIPANT = (userId) => `${USERS_PROJECTS_URL(userId)}/participant`;
+
+export const RECOMMENDED_USER_PROJECTS = (userId) => `${USERS_PROJECTS_URL(userId)}/newcontent`;
