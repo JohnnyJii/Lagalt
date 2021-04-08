ee2f7a1a76176dc5b17681782764c3b8ce4efa9b
jessemaa <mewwbae@gmail.com>
2021-04-08 14:40:33 +0300

    feat: project cards now show tags and skills in cards and modals

--- front/lagalt-app/src/components/views/profile-view/my-projects/my-projects-list/list-item/ProfileProjectsGridItemX.js
+++ front/lagalt-app/src/components/views/profile-view/my-projects/my-projects-list/list-item/ProfileProjectsGridItemX.js
@@ -1,7 +1,7 @@
 import './ProfileProjectsGridItemX.css';
 import React from 'react';
 import ProfileProjectsModalX from '../project-modal/ProfileProjectsModalX';
-import { Card } from 'react-bootstrap';
+import { Card, Badge } from 'react-bootstrap';
 import firebase from 'firebase/app';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import useApplications from '../../../../../../hooks/useApplications';
@@ -10,17 +10,29 @@ function ProfileProjectsGridItemX({ project = {} }) {
   const [modalShow, setModalShow] = React.useState(false);
   const auth = firebase.auth();
   const [user] = useAuthState(auth);
-  const { id, industry, progress, title, description, gitlink, user: owner } = project;
+  const { id, industry, progress, title, description, gitlink, user: owner, tags: tag, skills: skill } = project;
   const [applications, handleApplication] = useApplications(id, owner);
 
   return (
     <div style={{ margin: '20px', cursor: 'pointer' }}>
       <Card onClick={() => setModalShow(true)}>
-        <Card.Header>{industry ? industry : 'Industry'} <small>{progress ? progress : 'Progress'}</small></Card.Header>
+        <Card.Header>{industry ? industry : 'Industry'} <small><em>{progress ? progress : 'Progress'}</em></small>          {applications.length > 0 && <Badge variant="success">New Applicants!</Badge>}
+        </Card.Header>
         <Card.Body>
           <Card.Title>{title ? title : 'Title'}</Card.Title>
           <Card.Text>{description ? description : 'Description'}</Card.Text>
-          {applications.length > 0 && <p>NEW APPLICATIONS</p>}
+          <Card.Footer>
+            {tag.map(tag => (
+              <Badge variant="primary">
+                {tag ? tag : 'Tag'}
+              </Badge>
+            ))}
+            {skill.map(skills => (
+              <Badge variant="warning">
+                {skill ? skill : 'Skill'}
+              </Badge>
+            ))}
+          </Card.Footer>
         </Card.Body>
       </Card>
       <ProfileProjectsModalX
@@ -32,6 +44,8 @@ function ProfileProjectsGridItemX({ project = {} }) {
         gitlink={gitlink}
         creator={owner}
         show={modalShow}
+        skill={skill}
+        tag={tag}
         user={user}
         onHide={() => setModalShow(false)}
         applications={applications}
