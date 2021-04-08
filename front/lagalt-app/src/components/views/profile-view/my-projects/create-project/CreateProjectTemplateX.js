919e0d016b30745701c08ad48b63b7c57ae96840
jessemaa <mewwbae@gmail.com>
2021-04-08 13:38:11 +0300

    feat: add skills and tags to projects

--- front/lagalt-app/src/components/views/profile-view/my-projects/create-project/CreateProjectTemplateX.js
+++ front/lagalt-app/src/components/views/profile-view/my-projects/create-project/CreateProjectTemplateX.js
@@ -1,22 +1,6 @@
 import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import { SUGGESTIONS } from './ProjectTagSuccestions';
-import { WithContext as Tags } from 'react-tag-input';
 import './TagStyles.css';
-
-const suggestions = SUGGESTIONS.map((language) => {
-  return {
-    id: language,
-    text: language
-  };
-});
-
-// const KeyCodes = {
-//   comma: 188,
-//   enter: 13,
-// };
-
-// const delimeters = [KeyCodes.comma, KeyCodes.enter];
 
 class CreateProjectTemplateX extends Component {
   constructor(props) {
@@ -30,19 +14,17 @@ class CreateProjectTemplateX extends Component {
       progress: '',
       skills: [],
       tags: [],
-      suggestions: suggestions,
       gitlink: '',
       user: {
         id: this.dbuserid
       }
     };
-    this.handleDelete = this.handleDelete.bind(this);
+/*     this.handleDelete = this.handleDelete.bind(this);
     this.handleAddition = this.handleAddition.bind(this);
-    this.handleDrag = this.handleDrag.bind(this);
-    this.handleTagClick = this.handleTagClick.bind(this);
+    this.handleDrag = this.handleDrag.bind(this); */
   }
 
-  handleDelete(i) {
+/*   handleDelete(i) {
     const { tags } = this.state;
     this.setState({ tags: tags.filter((tag, index) => index !== i),
     });
@@ -55,16 +37,10 @@ class CreateProjectTemplateX extends Component {
   handleDrag(tag, currPos, newPos) {
     const tags = [...this.state.tags];
     const newTags = tags.slice();
-
     newTags.splice(currPos, 1);
     newTags.splice(newPos, 0, tag);
-
     this.setState({ tags: newTags });
-  }
-
-  handleTagClick(index) {
-    console.log(index);
-  }
+  } */
 
   changeHandler = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -72,132 +48,136 @@ class CreateProjectTemplateX extends Component {
 
   submitHandler = e => {
     e.preventDefault();
+    const form = {...this.state, skills: this.state.skills.split(','), tags: this.state.tags.split(',')};
+    this.addProject(form);
+  }
 
-  const form = this.state;
-  this.addProject(form);
-    }
+  render() {
+    const { title, description, gitlink, tags, skills } = this.state;
+    return (
+      <div>
+        <Form onSubmit={this.submitHandler}>
+          <Form.Label>Project Title</Form.Label>
+          <Form.Control name="title" type="text" placeholder="Project title" value={title} onChange={this.changeHandler} />
+          <br />
+          <Form.Label>Industry</Form.Label>
+          {['radio'].map((type) => (
+            <div key={`inline-${type}`} className="mb-3" onChange={this.changeHandler}>
 
-    render() {
-      const { title, description, gitlink, tags, skills, suggestions } = this.state;
-      return (
-        <div>
-          <Form onSubmit={this.submitHandler}>
-            <Form.Label>Project Title</Form.Label>
-            <Form.Control name="title" type="text" placeholder="Project title" value={title} onChange={this.changeHandler} />
-            <br />
-            <Form.Label>Industry</Form.Label>
-            {['radio'].map((type) => (
+              <Form.Check
+                inline name="industry"
+                label="Game Development"
+                type={type}
+                value={'Game Development'}
+                id={`inline-${type}-1`}
+              />
+
+              <Form.Check
+                inline name="industry"
+                label="Film"
+                type={type}
+                value={'Film'}
+                id={`inline-${type}-2`}
+              />
+
+              <Form.Check
+                inline name="industry"
+                label="Music"
+                type={type}
+                value={'Music'}
+                id={`inline-${type}-3`}
+              />
+
+              <Form.Check
+                inline name="industry"
+                label="Web Development"
+                type={type}
+                value={'Web Development'}
+                id={`inline-${type}-4`}
+              />
+            </div>
+          ))
+          }
+          <Form.Label>Progress</Form.Label>
+          {
+            ['radio'].map((type) => (
               <div key={`inline-${type}`} className="mb-3" onChange={this.changeHandler}>
 
                 <Form.Check
-                  inline name="industry"
-                  label="Game Development"
+                  inline name="progress"
+                  label="Founding"
                   type={type}
-                  value={'Game Development'}
-                  id={`inline-${type}-1`}
+                  value={'Founding'}
+                  id={`inline-${type}-5`}
                 />
 
                 <Form.Check
-                  inline name="industry"
-                  label="Film"
+                  inline name="progress"
+                  label="In Progress"
                   type={type}
-                  value={'Film'}
-                  id={`inline-${type}-2`}
+                  value={'In Progress'}
+                  id={`inline-${type}-6`}
                 />
 
                 <Form.Check
-                  inline name="industry"
-                  label="Music"
+                  inline name="progress"
+                  label="Stalled"
                   type={type}
-                  value={'Music'}
-                  id={`inline-${type}-3`}
+                  value={'Stalled'}
+                  id={`inline-${type}-7`}
                 />
 
                 <Form.Check
-                  inline name="industry"
-                  label="Web Development"
+                  inline name="progress"
+                  label="Completed"
                   type={type}
-                  value={'Web Development'}
-                  id={`inline-${type}-4`}
+                  value={'Completed'}
+                  id={`inline-${type}-8`}
                 />
+
               </div>
             ))
-            }
-            <Form.Label>Progress</Form.Label>
-            {
-              ['radio'].map((type) => (
-                <div key={`inline-${type}`} className="mb-3" onChange={this.changeHandler}>
-
-                  <Form.Check
-                    inline name="progress"
-                    label="Founding"
-                    type={type}
-                    value={'Founding'}
-                    id={`inline-${type}-5`}
-                  />
-
-                  <Form.Check
-                    inline name="progress"
-                    label="In Progress"
-                    type={type}
-                    value={'In Progress'}
-                    id={`inline-${type}-6`}
-                  />
-
-                  <Form.Check
-                    inline name="progress"
-                    label="Stalled"
-                    type={type}
-                    value={'Stalled'}
-                    id={`inline-${type}-7`}
-                  />
-
-                  <Form.Check
-                    inline name="progress"
-                    label="Completed"
-                    type={type}
-                    value={'Completed'}
-                    id={`inline-${type}-8`}
-                  />
-
-                </div>
-              ))
-            }
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Project Description</Form.Label>
-              <Form.Control name="description" as="textarea" rows={3} value={description} onChange={this.changeHandler} />
-            </Form.Group>
-              <div>
-              <Tags
-                  placeholder="Enter needed skills"
-                  skills={skills}
-                  // delimiters={delimiters}
-                  handleDelete={this.handleDelete}
-                  handleAddition={this.handleAddition}
-                  handleDrag={this.handleDrag}
-                  handleTagClick={this.handleTagClick}
-                />
-                <br />
-                <Tags
-                  placeholder="Tag the project"
-                  tags={tags}
-                  suggestions={suggestions}
-                  // delimiters={delimiters}
-                  handleDelete={this.handleDelete}
-                  handleAddition={this.handleAddition}
-                  handleDrag={this.handleDrag}
-                  handleTagClick={this.handleTagClick}
-                />
-              </div>
+          }
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Project Description</Form.Label>
+            <Form.Control name="description" as="textarea" rows={3} value={description} onChange={this.changeHandler} />
+          </Form.Group>
+          {/* <div>
+            <Tags
+              placeholder="Enter needed skills"
+              skills={skills}
+              // delimiters={delimiters}
+              handleDelete={this.handleDelete}
+              handleAddition={this.handleAddition}
+              handleDrag={this.handleDrag}
+              handleTagClick={this.handleTagClick}
+            />
             <br />
-            <Form.Label>Git Link</Form.Label>
-            <Form.Control name="gitlink" type="text" placeholder="Github repo link" value={gitlink} onChange={this.changeHandler} />
-            <br />
-            <Button variant="primary" type="submit">Create</Button>
-          </Form >
-        </div >
-      );
-    }
+            <Tags
+              placeholder="Tag the project"
+              tags={tags}
+              suggestions={suggestions}
+              // delimiters={delimiters}
+              handleDelete={this.handleDelete}
+              handleAddition={this.handleAddition}
+              handleDrag={this.handleDrag}
+              handleTagClick={this.handleTagClick}
+            />
+          </div> */}
+          <Form.Label>Needed Skills</Form.Label>
+          <Form.Control name="skills" placeholder="Skills separated with a comma (,)" value={skills} onChange={this.changeHandler}/>
+          <br />
+          <Form.Label>Related Tags and Keywords</Form.Label>
+          <Form.Control name="tags" placeholder="Tags separated with a comma (,)" value={tags} onChange={this.changeHandler}/>
+          <br />
+          <Form.Label>Git Link</Form.Label>
+          <Form.Control name="gitlink" type="text" placeholder="Github repo link" value={gitlink} onChange={this.changeHandler} />
+          <br />
+          <Button variant="primary" type="submit">Create</Button>
+        </Form >
+      </div >
+    );
+  }
 }
 
 export default CreateProjectTemplateX;
