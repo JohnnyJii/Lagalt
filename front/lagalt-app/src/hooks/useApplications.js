707ce6bc196e8026689d6fd18d57f2eac9c04385
veliValentine <nikkevanukas1995@gmail.com>
2021-04-08 20:01:31 +0300

    fix: jwt not updated in const header (#64)
    * feat: get recommended projects and show them on carusel
    
    * refactor: auth header variable in hook helper
    
    * fix: using autHeader const didn't update the JWT
    
    * fix: same changes

--- front/lagalt-app/src/hooks/useApplications.js
+++ front/lagalt-app/src/hooks/useApplications.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { APPLICATIONS_URL, HANDLE_APPLICATION_URL } from '../utils/serverUrls/serverUrl';
-import { authHeader } from './hookHelper';
 
 function useApplications(projectId, userId, projectOwnerId) {
   const [applications, setApplications] = useState([]);
   useEffect(() => {
     const fetchAndSetApplications = async () => {
-      const { data } = await axios.get(APPLICATIONS_URL(projectId), authHeader);
+      const { data } = await axios.get(APPLICATIONS_URL(projectId), {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('jwt')}`
+        }
+      });
       setApplications(data);
     };
     if ((projectId !== undefined) && ('' + userId === projectOwnerId)) {
@@ -16,7 +19,11 @@ function useApplications(projectId, userId, projectOwnerId) {
   }, [projectId, userId, projectOwnerId]);
 
   const handleApplication = async function (projectId, userId, accept) {
-    await axios.post(HANDLE_APPLICATION_URL(projectId, userId, accept), null, authHeader);
+    await axios.post(HANDLE_APPLICATION_URL(projectId, userId, accept), null, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`
+      }
+    });
     setApplications(applications.filter(application => {
       if (application.userId === userId && application.projectId === projectId) {
         return false;
